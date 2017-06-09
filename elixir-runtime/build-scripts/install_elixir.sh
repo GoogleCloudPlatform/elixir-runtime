4f1e17b0587002b9ca935895c32afe6842542646
Jeff Ching <chingor@google.com>
2017-06-09 09:34:12 -0700

    build multiple versions of elixir and erlang in cloudbuild

--- elixir-runtime/build-scripts/install_elixir.sh
+++ elixir-runtime/build-scripts/install_elixir.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
-curl https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_${ELIXIR_VERSION}-1~debian~jessie_all.deb \
+# https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_1.4.4-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_1.3.4-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_1.2.6-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_1.1.1-2~debian~jessie_amd64.deb
+
+curl https://packages.erlang-solutions.com/erlang/elixir/FLAVOUR_2_download/elixir_${ELIXIR_VERSION}~debian~jessie_amd64.deb \
     -o /tmp/elixir.deb
 
 dpkg -i /tmp/elixir.deb
--- erlang-runtime/build-scripts/install_erlang.sh
+++ erlang-runtime/build-scripts/install_erlang.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
+# https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.3-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.2.3-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.1.5-1~debian~jessie_amd64.deb
+# https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.0.7-1~debian~jessie_amd64.deb
+
 # install erlang
-curl https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_${ERLANG_VERSION}-1~debian~jessie_amd64.deb \
+curl https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_${ERLANG_VERSION}~debian~jessie_amd64.deb \
     -o /tmp/erlang.deb
 dpkg -i /tmp/erlang.deb
 rm /tmp/erlang.deb
