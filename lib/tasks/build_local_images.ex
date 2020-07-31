5f85022d90fbb02e4f958b9be6d455f637985c98
Daniel Azuma <dazuma@google.com>
2020-07-31 13:44:15 -0700

    Update versions

--- lib/tasks/build_local_images.ex
+++ lib/tasks/build_local_images.ex
@@ -20,13 +20,13 @@ defmodule Mix.Tasks.BuildLocalImages do
   @shortdoc "Build images locally."
 
   @os_name "ubuntu18"
-  @prebuilt_erlang_versions ["22.2.8"]
-  @base_erlang_version "22.2.8"
-  @base_elixir_version "1.10.2-otp-22"
+  @prebuilt_erlang_versions ["22.3.4.4"]
+  @base_erlang_version "22.3.4.4"
+  @base_elixir_version "1.10.4-otp-22"
   @old_distillery_elixir_version "1.8.2-otp-22"
   @asdf_version "0.7.8"
-  @nodejs_version "12.16.1"
-  @gcloud_version "287.0.0"
+  @nodejs_version "12.18.3"
+  @gcloud_version "303.0.0"
 
   @prebuilt_erlang_image_prefix "elixir-prebuilt-erlang-"
 
